2066fec2ba3476557be1313687504f2be8858940
TivyLecca <155016556+TivyLecca@users.noreply.github.com>
2025-08-12 17:34:29 -0500

    Update obtener_descargas.R

--- R/obtener_descargas.R
+++ R/obtener_descargas.R
@@ -30,7 +30,7 @@ obtener_descargas = function(dir_data = dir_data, descargas = NULL, empresas = e
 
     }else{
       for(ii in archivos){
-        files_descargas = read.csv(ii, fileEncoding = "latin1")
+        files_descargas = read.csv(ii,sep";", fileEncoding = "latin1")
         all_descargas   = rbind(files_descargas, all_descargas)
         names(all_descargas) = c( "X","N.","ID" ,"Puerto" ,"Planta", "Armador", "Embarcacion", "CBO" ,
                                   "Matricula" ,"Especie","Tolva", "Nro..Reporte", "TM.Descargadas","TM.Declarado","Acta.de.EIP","Fecha" ,
